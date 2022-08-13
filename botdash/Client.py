68491c585d9ceeb31a5c0ef65241c09c7592ae0c
ProDev <70071615+ProDev76@users.noreply.github.com>
2022-08-13 21:58:24 +0200

    oopsie woopsie uwu i made a fucky wucky HAHAHAHAHHAHH python

--- botdash/Client.py
+++ botdash/Client.py
@@ -179,8 +179,8 @@ class Client:
             return func
         return decorator
     def emit(self, event: str, data):
-        for event in self.events:
-            if event["event"] == event["event"]:
+        for eventFromLoopBecauseIsuckAtPythonAndPylintWillProbablyComplainAboutCamelCaseAnd_now_snake_case_HAHAHAHAH in self.events:
+            if eventFromLoopBecauseIsuckAtPythonAndPylintWillProbablyComplainAboutCamelCaseAnd_now_snake_case_HAHAHAHAH["event"] == event["event"]:
                 asyncio.run(event["func"](data))
     def set(self, guild_id: str, key: str, value: str):
         self.socket.emit("set", {
